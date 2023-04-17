94f3f0c35f7d9c109e3dde1f8ed4805065d8a3df
micaelsimoes <micaelsimoes@gmail.com>
2023-04-17 15:42:50 +0100

    fl_relax added to NetworkParameters class.

--- network_parameters.py
+++ network_parameters.py
@@ -14,6 +14,7 @@ class NetworkParameters:
         self.rg_curt = False
         self.l_curt = False
         self.ess_relax = True
+        self.fl_relax = True
         self.enforce_vg = False
         self.slack_line_limits = False
         self.slack_voltage_limits = False
@@ -58,6 +59,8 @@ def _read_network_parameters_from_file(parameters, filename):
                     parameters.l_curt = read_bool_parameter(tokens[1])
                 elif param_type == 'ess_relax':
                     parameters.ess_relax = read_bool_parameter(tokens[1])
+                elif param_type == 'fl_relax':
+                    parameters.fl_relax = read_bool_parameter(tokens[1])
                 elif param_type == 'enforce_vg':
                     parameters.enforce_vg = read_bool_parameter(tokens[1])
                 elif param_type == 'slack_line_limits':
